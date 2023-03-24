bba9ce7f4c7986bcbe221e7b70fac9edcfb06fc2
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-03-24 17:56:26 +0200

    Modified modal

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,28 +1,31 @@
 import { AnimatePresence, motion } from "framer-motion";
-import { useRef } from "react";
 
-const Modal = ({ text, isOpen, setIsOpen }) => {
-    const ref = useRef(null)
-    if(ref.current){
-        setIsOpen(false)
-    }
+const Modal = ({ children, isOpen, setIsOpen }) => {
+  
+
   return (
     <AnimatePresence>
       {isOpen && (
         <>
-          <motion.div 
-          initial={{opacity: 0}}
-          animate={{opacity: 1}}
-          exit={{opacity: 0, delay: 0.1}}
-          transition={{duration: 0.3, ease: "easeInOut"}}
-          className="fixed w-full h-screen bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0"></motion.div>
-          <motion.div 
-          initial={{opacity: 0, x: -50, delay: 0.1}}
-          animate={{opacity: 1}}
-          exit={{opacity: 0, x: -50}}
-          transition={{duration: 0.3, ease: "linear"}}
-          className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-4">
-            {text}
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0, delay: 0.2 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            onClick={()=> setIsOpen(false)}
+            className="fixed w-full h-screen flex justify-center items-center bg-black backdrop-filter backdrop-blur-md bg-opacity-60 top-0 left-0 right-0 bottom-0 cursor-pointer"
+          >
+
+          <motion.div
+            initial={{ opacity: 0}}
+            animate={{ opacity: 1, y: -30 }}
+            exit={{ opacity: 0, y: 30 }}
+            transition={{ duration: 0.3, ease: "linear" }}
+            onClick={e => e.stopPropagation()}
+            className="max-w-[400px] bg-white p-7 space-y-2 text-center cursor-text rounded-md"
+          >
+            {children}
+          </motion.div>
           </motion.div>
         </>
       )}
